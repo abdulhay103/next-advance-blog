0f90294815b6ae0e2c74e72f5a9ed76cf70f9792
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-20 12:05:15 +0600

    working

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ const page = async () => {
   return (
     <div className="">
       <h1 className="py-3 text-lg font-bold">Server side API Rendaring</h1>
+      <h1>{process.env.API_KEY}</h1>
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //   basePath: "/abc",
+  env: {
+    API_KEY: "Hello",
+  },
+  async headers() {
+    return [{ source: "/path*" }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
