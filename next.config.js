c36f9b64f3b052498905147b6ca6457427e5a099
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-22 12:17:58 +0600

    Requirment done

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   // basePath: "/abc",
   env: {
-    API_KEY: "Hello",
-    // BASE_URL: "https://basic-blog.teamrabbil.com/api/",
     NEWEST_POSTS: "https://basic-blog.teamrabbil.com/api/post-newest",
     POST_LIST: "https://basic-blog.teamrabbil.com/api/post-list/1",
     POST_DETAILS: "https://basic-blog.teamrabbil.com/api/post-details",
