5857b61815150001fe5c30d98a2a7ea1e144cef7
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-23 12:14:27 +0600

    checked

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-// "use client";
-// import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+"use client";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -16,12 +16,12 @@ export default function RootLayout({ children }) {
         <div className=" bg-gradient-to-br from-green-50 via-orange-50 to-sky-100 w-full min-h-screen">
           <Header />
           <div className=" container mx-auto py-20"> {children}</div>
-          {/* <ProgressBar
+          <ProgressBar
             height="3px"
             color="#F97316"
             options={{ showSpinner: false }}
             shallowRouting
-          /> */}
+          />
           <Footer />
         </div>
       </body>
