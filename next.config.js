2d68622909f767c7b75542f14a21070a081e2333
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-23 12:31:10 +0600

    rechecked

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEWEST_POSTS: "https://basic-blog.teamrabbil.com/api/post-newest",
+    POST_LIST: "https://basic-blog.teamrabbil.com/api/post-list/1",
+    POST_DETAILS: "https://basic-blog.teamrabbil.com/api/post-details",
+  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "roar.media",
+  //     },
+  //   ],
+  // },
+  // distDir: "build",
+};
 
 module.exports = nextConfig;
